974d2e22bea4d2a3a4b4e9c57c64f486accb1c89
Panjamapong Sermsawatsri <panjmp@gmail.com>
2016-05-15 03:39:45 +0700

    change port

--- socket-server.js
+++ socket-server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(8998);
 
 app.listen(process.env.API_PORT, () => console.log('Listening...'));
 

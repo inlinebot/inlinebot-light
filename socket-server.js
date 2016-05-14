127bcd6809b97f7014733713e597441748363608
Panjamapong Sermsawatsri <panjmp@gmail.com>
2016-05-15 03:33:34 +0700

    change location

--- socket-server.js
+++ socket-server.js
@@ -5,15 +5,14 @@ const io = require('socket.io')(server);
 
 app.listen(process.env.API_PORT, () => console.log('Listening...'));
 
+app.get('/:contextId/:mode', (req, res) => {
+  io.to(req.params.contextId).emit('set', req.params.mode);
+  res.send(req.params);
+});
+
 io.on('connection', function(socket){
-  socket.join('some room');
   socket.on('join-room', (room) => {
     socket.join(room);
     console.log('joined', room);
   });
-});
-
-app.get('/:contextId/:mode', (req, res) => {
-  io.to(req.params.contextId).emit('set', req.params.mode);
-  res.send(req.params);
 });

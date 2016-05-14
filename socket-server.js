0c133b96c82ea7048c995c3e255de541c94c31e1
Panjamapong Sermsawatsri <panjmp@gmail.com>
2016-05-15 03:00:38 +0700

    return resp

--- socket-server.js
+++ socket-server.js
@@ -7,11 +7,13 @@ io.on('connection', function(socket){
   socket.join('some room');
   socket.on('join-room', (room) => {
     socket.join(room);
+    console.log('joined', room);
   });
 });
 
 app.get('/:contextId/:mode', (req, res) => {
   io.to(req.params.contextId).emit('set', req.params.mode);
+  res.send(req.params);
 });
 
 server.listen(process.env.API_PORT, () => console.log('Listening...'));
